c4a9e4ca66221ce60618b68066a8148c929adff1
KGK8 <63961693+KGK8@users.noreply.github.com>
2021-04-06 13:50:08 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ window.addEventListener('deviceorientation',function(e){
     const y = e.gamma;
     const z = e.alpha;
 
-    document.getElementsByClassName('xtxt').innerText = "X-Axis = " + x;
-    document.getElementsByClassName('ytxt').innerText = "Y-Axis = " + y;
-    document.getElementsByClassName('ztxt').innerText = "Z-Axis = " + z;
+    document.getElementsByClassName('xtxt')[0].innerText = "X-Axis = " + x;
+    document.getElementsByClassName('ytxt')[0].innerText = "Y-Axis = " + y;
+    document.getElementsByClassName('ztxt')[0].innerText = "Z-Axis = " + z;
 
 
 
