6595e349b58d5780e1f75bea25123ce86f3d4698
KGK8 <63961693+KGK8@users.noreply.github.com>
2021-04-06 14:01:34 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 window.addEventListener('deviceorientation',function(e){
-    const x = e.beta;
-    const y = e.gamma;
-    const z = e.alpha;
+    const x = Math.round(e.beta);
+    const y = Math.round(e.gamma);
+    const z = Math.round(e.alpha);
 
     document.getElementsByClassName('xtxt')[0].innerText = "X-Axis = " + x;
     document.getElementsByClassName('ytxt')[0].innerText = "Y-Axis = " + y;
     document.getElementsByClassName('ztxt')[0].innerText = "Z-Axis = " + z;
 
+    //Parallax Effect
 
+    document.getElementsByClassName('rocket')[0].style.transition = `translateY(-${y}%)`;
 
 });
